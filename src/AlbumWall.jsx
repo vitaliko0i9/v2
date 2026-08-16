6a429ed0f66ffed339fb0b4b857bd2869e4e5d3b
Vitaliy <vetalik.gavrysh@gmail.com>
2026-08-16 17:59:20 +0300

    SAMEWALL

--- src/AlbumWall.jsx
+++ src/AlbumWall.jsx
@@ -4,59 +4,63 @@ import axios from "axios";
 function AlbumWall() {
 
     const [ albums, setAlbums ] = useState([]);
-    
+
+
     useEffect(() => {
         const getAlbums = async () => {
-            const requests = artists.map(artist => 
-                axios.get(`https://itunes.apple.com/search?term=${artist}&entity=album&limit=5`)
-            )
-
-            const responses = await Promise.all(requests)
-            const AllAlbums = responses.flatMap(
-                response => response.data.results
-            );
-            setAlbums(AllAlbums)
+            const response = await fetch("/src/albums.json");
+            const data = await response.json();
+            setAlbums(data);
         };
-        getAlbums()
+        getAlbums();
     }, []);
- 
-    return (
-        <div className="album-wall">
-            {albums.map(album => {
-                const size = Math.floor(Math.random() * 155) + 100;
-                return (
-                    <a
-                        className="falling-album"
-                        href={album.collectionViewUrl}
-                        key={album.collectionId}
-                        target="_blank"
-                        style={{
-                            width: `${size}px`,
-                            height: `${size}px`,
-                        }}
-                    >
-                        <img
-                            src={album.artworkUrl100.replace("100x100", "600x600")}
-                            alt={album.collectionName}
-                        />
-                    </a>
-                );
-            })}
-        </div>
-    );
-}
-    const artists = [
-        "Kendrick Lamar",
-        "Drake",
-        "The Weeknd",
-        "Tyler the Creator",
-        "Taylor Swift",
-        "Frank Ocean",
-        "Travis Scott",
-        "Billie Eilish",
-        "Adele",
-        "Kanye West",
-        "Playboi Carti"
-    ];
 
+    // useEffect(() => {
+    //     const getAlbums = async () => {
+    //         const requests = artists.map(artist => 
+    //             axios.get(`https://itunes.apple.com/search?term=${artist}&entity=album&limit=7`)
+    //         )
+
+    //         const responses = await Promise.all(requests)
+    //         const AllAlbums = responses.flatMap(
+    //             response => response.data.results
+    //         );
+    //         setAlbums(AllAlbums)
+    //     };
+    //     getAlbums()
+    // }, []);
+    return (
+        <div className="wall-container">
+    <div className="album-wall">
+        {albums.map((album, index) => (
+            <a
+                className="falling-album"
+                href={album.collectionViewUrl}
+                key={`${album.collectionId}-${index}`}
+            >
+                <img
+                    src={album.artworkUrl100.replace("100x100", "600x600")}
+                    alt={album.collectionName}
+                />
+            </a>
+        ))}
+    </div>
+
+    <div className="album-wall">
+        {albums.map((album, index) => (
+            <a
+                className="falling-album"
+                href={album.collectionViewUrl}
+                key={`second-${album.collectionId}-${index}`}
+            >
+                <img
+                    src={album.artworkUrl100.replace("100x100", "600x600")}
+                    alt={album.collectionName}
+                />
+            </a>
+        ))}
+    </div>
+</div>
+    )
+}
 export default AlbumWall;
--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
   return(
     <>
+    <AlbumWall />
     <Header></Header>
     <Body></Body>
     </>
